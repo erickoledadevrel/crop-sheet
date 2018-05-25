808c287b8e7874f4fc545875313dcf1eb02226b5
Eric Koleda <ekoleda@google.com>
2018-05-25 13:30:39 -0400

    Cleaning up some unused config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,6 @@ module.exports = {
   "parserOptions": {
     "ecmaVersion": 6,
   },
-  "globals": {
-    "HtmlService": false,
-    "Logger": false,
-    "OAuth2": false,
-    "UrlFetchApp": false,
-  },
   "rules": {
     "comma-dangle": "off",
     "no-var": "off",
